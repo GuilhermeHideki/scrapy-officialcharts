711df68cca1ed8a7519f629286f73c6a23e85652
Guilherme Danno <Guilherme.danno@gmail.com>
2016-05-26 23:33:37 -0300

    remove variable and pep8

--- officialcharts/spiders/top40spider.py
+++ officialcharts/spiders/top40spider.py
@@ -1,6 +1,7 @@
 import scrapy
 from officialcharts.items import OfficialchartsItem
 
+
 class Top40Spider(scrapy.Spider):
     """docstring for Top40Spider"""
     name = "Top40Singles"
@@ -10,19 +11,18 @@ class Top40Spider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        j = 0
         for i, x in enumerate(response.xpath(
                 "//html/body/div/div/article/div/div/section/table/tr")):
             # The 1st <tr> is the header
             # The items are 5 <tr>'s apart
             # For each 30 items add a <tr> e.g. (30 * 5 + 1)
             if (i % 151) % 5 == 2:
-                j += 1
                 item = OfficialchartsItem()
 
                 data = [x.xpath("//tr[{}]/td[3]/div/div/div/a/text()".format(i))
                         .extract()][0]
-                item['pos'] = j
+                item['pos'] = x.xpath("//tr[{}]/td[1]/span/text()".format(i))\
+                    .extract()[0]
                 item['artist'] = data[1]
                 item['title'] = data[0]
 
